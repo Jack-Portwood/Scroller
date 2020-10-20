678e07004085f658bf021c519984e525742b899d
onitwin <onitwin@gmail.com>
2020-10-20 14:51:19 +0100

    updated file for testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Joseph 
+          Thanks Jack!
         </p>
         <a
           className="App-link"
