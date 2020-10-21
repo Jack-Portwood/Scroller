c078e1eb67f47a1e831f9c978b2e0c4b557ed79f
onitwin <onitwin@gmail.com>
2020-10-21 10:52:02 +0100

    update to individual

--- src/components/ScrollChoices.js
+++ src/components/ScrollChoices.js
@@ -2,13 +2,19 @@ import React ,{useEffect,useState,Fragment} from 'react'
 import SingleScrollItem from "./SingleScrollItem"
 
 //state to go here
+//arrayOfPhrases objects
+//selectedPhrase
+
+//phraseObject{id:uuid,text:phrase,speed:number,css:css}
+
+const [phrases,setPharases]=useState([{id:0,text:"this is the first item",speed:5,css:"placeholder"},{id:1,text:"this is the first item",speed:10,css:"placeholder"},{id:2,text:"this is the first item",speed:3,css:"placeholder"}])
 
 
 const ScrollChoices=()=>{
   return(
     <Fragment>
     <h2>Hello from the Scroll Choices component</h2>
-    <SingleScrollItem/>
+    <SingleScrollItem phrases={phrases}/>
     </Fragment>
   )
 }
--- src/components/SingleScrollItem.js
+++ src/components/SingleScrollItem.js
@@ -2,10 +2,13 @@ import React ,{useEffect,useState,Fragment} from 'react'
 import Options from "./Options"
 
 
-const SingleScrollItem=()=>{
+const SingleScrollItem=({phrases})=>{
+  const newphrases=phrases
+
   return(
     <Fragment>
     <h2>Hello from the SingleScrollItem component</h2>
+    {newphrases}
     <Options/>
     </Fragment>
   )
