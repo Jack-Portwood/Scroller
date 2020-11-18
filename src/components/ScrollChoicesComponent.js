c5fcb5c12010cfaa651f42a59087aa62bcceaef6
onitwin <onitwin@gmail.com>
2020-11-18 12:56:11 +0000

    updated form

--- src/components/ScrollChoicesComponent.js
+++ src/components/ScrollChoicesComponent.js
@@ -2,6 +2,7 @@ import React ,{useState,Fragment} from 'react'
 import ScrollScreen from "./ScrollScreen"
 import DeviceOrientation, {Orientation} from 'react-screen-orientation'
 import { isCompositeComponent } from 'react-dom/test-utils';
+import uuid from 'react-uuid'
 
 
 
@@ -20,7 +21,7 @@ function updatePhrases(event) {
 // passed to modal to create new phrases and to push into phrases state.
 function createPhrase(event) {
   event.preventDefault();
-  const newPhrase={id:7,text: event.target.phraseInput.value, speed:event.target.updateSpeed.value, css: "placeholder"}
+  const newPhrase={id:uuid(),text: event.target.phraseInput.value, speed:event.target.updateSpeed.value, css: "placeholder"}
   setPhrases([...phrases,newPhrase])
   console.log("Hello");
 }
@@ -33,15 +34,15 @@ function deletePhrases(event) {
 
 //list of phrases objects.
 const [phrases, setPhrases] = useState([
-  { id: 0, text: "this is the first item", speed: 2, css: "placeholder" },
+  { id:uuid(), text: "this is the first item", speed: 2, css: "placeholder" },
   {
-    id: 1,
+    id:uuid(),
     text:
       " Hello World, I am still in development view on your mobile device",
-    speed: 5,
+    speed:5,
     css: "placeholder",
   },
-  { id: 2, text: "this is the third item", speed: 15, css: "placeholder" },
+  { id:uuid(), text: "this is the third item", speed: 15, css: "placeholder" },
 ]);
 
 // state for selected phrase to be passed into rotated component.
@@ -51,7 +52,7 @@ let [selectedPhrase, setSelectedPhrase] = useState(phrases[1]);
 //state of selected phrase
 function usePhrase(event) {
   event.preventDefault();
-  let newValue = parseInt(event.target.value)
+  let newValue = (event.target.value)
   for (let i = 0; i < phrases.length; i++){
       if(phrases[i].id === newValue) {
         setSelectedPhrase(phrases[i])
